d92ce95e3047dec0f772eb8ef9499b1608fc2e61
Ross Gravem <rgravem@yahoo.com>
2016-10-19 10:44:04 -0500

    first commit - server up

---
+++ server/app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+var bodyParser = require("body-parser");
+var urlencodedParser = bodyParser.urlencoded( { extended: true } );
+var bpJason = bodyParser.json();
+var path = require('path');
+var port = process.env.PORT || 3000;
+
+
+app.use( express.static( 'public' ) );
+
+app.listen(port, function(){
+  console.log('server up on 3000');
+});
+
+
+app.get("/*", function(req,res){
+    var file = req.params[0] || "/views/index.html";
+    res.sendFile(path.join(__dirname, "../public/", file));
+});

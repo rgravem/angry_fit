2a456d4ced4dd31859673bd011117a59e4602efd
Ross Gravem <rgravem@yahoo.com>
2016-10-24 13:08:01 -0500

    added buttons to customer type

--- public/scripts/controllers/customerTypeController.js
+++ public/scripts/controllers/customerTypeController.js
@@ -1,4 +1,14 @@
-myApp.controller("customerTypeController", ['$scope', '$http', function($scope, $http){
+myApp.controller("customerTypeController", ['$scope', '$location', '$http', function($scope, $location, $http){
   console.log('In customerTypeController');
+
+$scope.newCustomer = function(){
+  console.log('new customer hit');
+  $location.path('/newCustomer');
+};
+
+$scope.existingCustomer = function(){
+  console.log('exisiting customer clicked');
+  $location.path('/existingCustomer');
+};
 
 }]);//end customerTypeController

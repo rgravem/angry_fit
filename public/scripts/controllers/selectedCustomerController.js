f95b7f1b657508fac5cfb3b42fcb2d123437fbc7
Justin O'Connor <justin.oc03@gmail.com>
2016-10-24 15:17:49 -0500

    update route working

--- public/scripts/controllers/selectedCustomerController.js
+++ public/scripts/controllers/selectedCustomerController.js
@@ -1,4 +1,26 @@
 myApp.controller("selectedCustomerController", ['$scope', '$http', function($scope, $http){
   console.log('In selectedCustomerController');
 
+  $scope.editExistingCustomer = function () {
+    var editCustomerObject = {
+      firstName: $scope.firstName,
+      lastName:$scope.lastName,
+      email:$scope.email,
+      phoneNumber:$scope.phoneNumber,
+      streetAddress:$scope.streetAddress,
+      unitNumber:$scope.unitNumber,
+      city:$scope.city,
+      state:$scope.state,
+      zip:$scope.zip
+    };
+
+    $http({
+      method: 'PUT',
+      url: '/editExistingCustomer',
+      data: editCustomerObject
+    }).then(function(editCustomerResponse){
+      console.log('success from DB', editCustomerResponse);
+    });
+  };
+
 }]);//end selectedCustomerController
--- server/app.js
+++ server/app.js
@@ -8,13 +8,11 @@ var port = process.env.PORT || 3000;
 var pg = require('pg');
 var connectionString = 'postgres://localhost:5432/angryFit';
 
-
 app.use( express.static( 'public' ) );
 
 app.listen(port, function(){
   console.log('server up on 3000');
 });
-
 
 ////////////////////Get Existing Customers from DB/////////////////////////////
 app.get('/getExistingCustomers', function(req, res){
@@ -83,6 +81,56 @@ app.post( '/addNewCustomer', urlencodedParser, function( req, res ){
   });// end pg connect
 });//end of post
 
+/////////////////////////Edit Existing Customer in DB///////////////////////////
+app.post( '/editExistingCustomer', urlencodedParser, function( req, res ){
+  console.log( 'in editExistingCustomer' );
+  editCustomerInfo = req.body;
+  console.log('editCustomerInfo:', editCustomerInfo);
+  //assemble object to send
+  var firstName = editCustomerInfo.firstName;
+  var lastName = editCustomerInfo.lastName;
+  var email = editCustomerInfo.email;
+  var phoneNumber = editCustomerInfo.phoneNumber;
+  var streetAddress = editCustomerInfo.streetAddress;
+  var unitNumber = editCustomerInfo.unitNumber;
+  var city = editCustomerInfo.city;
+  var state = editCustomerInfo.state;
+  var zip = editCustomerInfo.zip;
+  var id = editCustomerInfo.id; //we need to define where this comes from, it will likely be from a get route that finds that specific customer's information
+
+  pg.connect(connectionString, function(err, client, done){
+    //check for error
+    if(err){
+      console.log(err);
+    }//end error check
+    else{
+      console.log("connected to DB");
+      //array to hold results
+      var editCustomerInfoToSend = [];
+      //send update to DB
+      // client.query('UPDATE customers (firstName, lastName, email, phoneNumber, streetAddress, unitNumber, city, state, zip) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9);', [firstName, lastName, email, phoneNumber, streetAddress, unitNumber, city, state, zip]);
+
+      client.query('UPDATE customers SET firstName = ($1), lastName = ($2), email = ($3), phoneNumber = ($4), streetAddress = ($5), unitNumber = ($6), city = ($7), state = ($8), zip = ($9) WHERE id = ($10)', [firstName, lastName, email, phoneNumber, streetAddress, unitNumber, city, state, zip, id]);
+
+      //Query the DB
+      var queryResults = client.query('SELECT * From customers');
+      //run for each row in the query
+      queryResults.on("row", function(row){
+        editCustomerInfoToSend.push(row);
+      }); //end of row
+      queryResults.on('end', function(){
+        //we're done
+        done();
+        //return result as JSON version of array
+        return res.json(editCustomerInfoToSend);
+
+      });//end of end
+    }// end of else
+  });// end pg connect
+
+});
+
+//////////////////////////////generic app.get///////////////////////////////////
 app.get("/*", function(req,res){
     var file = req.params[0] || "/views/index.html";
     res.sendFile(path.join(__dirname, "../public/", file));

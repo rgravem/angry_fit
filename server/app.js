e0379849ee73cfb587f856cc31d321e874e52d38
Ross Gravem <rgravem@yahoo.com>
2016-10-25 11:21:46 -0500

    firebase auth working

--- server/app.js
+++ server/app.js
@@ -1,3 +1,4 @@
+const firebase = require('firebase');
 var express = require('express');
 var app = express();
 var bodyParser = require("body-parser");
@@ -9,10 +10,38 @@ var pg = require('pg');
 var connectionString = 'postgres://localhost:5432/angryFit';
 
 app.use( express.static( 'public' ) );
+app.use(bpJason);
+app.use(urlencodedParser);
 
 app.listen(port, function(){
   console.log('server up on 3000');
 });
+
+firebase.initializeApp({
+  serviceAccount: "./server/firebase-service-account.json",
+  databaseURL:"https://angryfit-90fce.firebaseio.com/"
+});
+
+app.get("/privateData", function(req, res){
+
+  /* This is where the magic happens. We pull the idtoken off of the request,
+  verify it against our private_key, and then we return the decodedToken */
+  firebase.auth().verifyIdToken(req.headers.id_token).then(function(decodedToken) {
+    /* Whatever you do in here is protected by your authorization.
+    WARNING: So far you are returning secret data to ANYONE who is logged in
+    there is still more work to be done if you want to implement roles
+    You can use the decodedToken and some logic to do that. */
+
+    console.log(decodedToken); // Here you can see the information firebase gives you about the user
+    res.send("Welcome back " + decodedToken.name + " you are now logged in!");
+  })
+  .catch(function(error) {
+    // If the id_token isn't right, you end up in this callback function
+    res.send("No secret data for you!");
+  });
+
+});
+
 
 ////////////////////Get Existing Customers from DB/////////////////////////////
 app.get('/getExistingCustomers', function(req, res){

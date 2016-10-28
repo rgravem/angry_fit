aa616c7739c870457f8cbae756bc85aecf976011
Ross Gravem <rgravem@yahoo.com>
2016-10-28 11:13:22 -0500

    selected customer populated

--- public/scripts/controllers/selectedCustomerController.js
+++ public/scripts/controllers/selectedCustomerController.js
@@ -1,6 +1,16 @@
 myApp.controller("selectedCustomerController", ['$scope', '$http', function($scope, $http){
   console.log('In selectedCustomerController');
 
+  $scope.newBikeStart = function(){
+    console.log('newbike');
+     $scope.newBikeButton = !$scope.newBikeButton;
+  };
+
+  $scope.otherType = false;
+  $scope.otherSelected = function(){
+    $scope.otherType = !$scope.otherType;
+  };
+
   $scope.editExistingCustomer = function () {
     var editCustomerObject = {
       firstName: $scope.firstName,

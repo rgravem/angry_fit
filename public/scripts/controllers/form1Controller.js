b36e414b99356e4e0b633a0fe7f3ce7cbea47464
Brent Reno <brent.reno@gmail.com>
2016-10-28 11:08:37 -0500

    form 1 disabled on submit, re-enabled on update and format updates to match other forms

--- public/scripts/controllers/form1Controller.js
+++ public/scripts/controllers/form1Controller.js
@@ -1,6 +1,10 @@
 myApp.controller("form1Controller", ['$scope', '$http', function($scope, $http){
   console.log('In form1Controller');
+      // set form to edit and submit status
+      //show submit button, hide update and pdf
   $scope.showHideSubmitFormOne = true;
+  //keep all input fields active
+  $scope.submittedOne=false;
   $scope.addFormOne = function () {
 
     console.log('in AddFormOne button click');
@@ -29,14 +33,22 @@ myApp.controller("form1Controller", ['$scope', '$http', function($scope, $http){
     };
 
     console.log('formOneObject to send to DB:', formOneObject);
+    //hide submit, show update and pdf
     $scope.showHideSubmitFormOne = false;
+    //disable input fields
+    $scope.submittedOne = true;
 
-    $http({
-      method: 'POST',
-      url: '/addFormOne',
-      data: formOneObject
-    }).then(function(formOneObject){
-      console.log('success from server', formOneObject);
-    });
+    // $http({
+    //   method: 'POST',
+    //   url: '/addFormOne',
+    //   data: formOneObject
+    // }).then(function(formOneObject){
+    //   console.log('success from server', formOneObject);
+    // });
   };
+    $scope.updateFormOne = function(){
+      //reset form to submit staus
+      $scope.submittedOne = false;
+      $scope.showHideSubmitFormOne = true;
+    };
 }]);//end form1Controller

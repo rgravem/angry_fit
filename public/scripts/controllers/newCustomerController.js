cb38ebc00ba04d051179d1f1c01c408e087a8d17
Ross Gravem <rgravem@yahoo.com>
2016-10-28 16:27:34 -0500

    linked first 2 pages

--- public/scripts/controllers/newCustomerController.js
+++ public/scripts/controllers/newCustomerController.js
@@ -18,14 +18,14 @@ myApp.controller("newCustomerController", ['$scope', '$http', '$location', funct
     console.log('json obj:', obj);
     console.log('newCustomerObject to send to DB:', newCustomerObject);
 
-    // $http({
-    //   method: 'POST',
-    //   url: '/addNewCustomer',
-    //   data: newCustomerObject
-    // }).then(function(newCustomerResponse){
-    //   console.log('success from server', newCustomerResponse);
-    //   $location.path('/selectedCustomer');
-    // });
+    $http({
+      method: 'POST',
+      url: '/addNewCustomer',
+      data: newCustomerObject
+    }).then(function(newCustomerResponse){
+      console.log('success from server', newCustomerResponse);
+      $location.path('/selectedCustomer');
+    });
   };
 
 }]);//end newCustomerController
